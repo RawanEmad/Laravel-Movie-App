0facea4033a55bd034b35aa0164fdb8b96229974
Rawan Emad <rawan.emadnegm@gmail.com>
2021-09-13 15:08:42 +0200

    Adding Actors

--- app/Http/Controllers/ActorsController.php
+++ app/Http/Controllers/ActorsController.php
@@ -4,52 +4,28 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
-use App\ViewModels\MovieViewModel;
-use App\ViewModels\MoviesViewModel;
+use App\ViewModels\ActorViewModel;
+use App\ViewModels\ActorsViewModel;
 
 
-
-class MoviesController extends Controller
+class ActorsController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index($page = 1)
     {
-        $popularMovies = HTTP::withToken(config('services.tmdb.token'))
-            ->get('https://api.themoviedb.org/3/movie/popular')
+        abort_if($page > 500, 204);
+
+        $popularActors = HTTP::withToken(config('services.tmdb.token'))
+            ->get('https://api.themoviedb.org/3/person/popular?page='.$page)
             ->json()['results'];
 
-        $nowPlayingMovies = HTTP::withToken(config('services.tmdb.token'))
-            ->get('https://api.themoviedb.org/3/movie/now_playing')
-            ->json()['results'];
+        $viewModel = new ActorsViewModel($popularActors, $page);
 
-        $genres = HTTP::withToken(config('services.tmdb.token'))
-            ->get('https://api.themoviedb.org/3/genre/movie/list')
-            ->json()['genres'];
-
-        // $genres = collect($genresArray)->mapWithKeys(function ($genre) {
-        //     return [$genre['id'] => $genre['name']];
-        // });
-
-
-        //dump($nowPlayingMovies);
-
-        // return view('index', [
-        //     'popularMovies' => $popularMovies,
-        //     'nowPlayingMovies' => $nowPlayingMovies,
-        //     'genres' => $genres,
-        // ]);
-
-        $viewModel = new MoviesViewModel(
-            $popularMovies,
-            $nowPlayingMovies,
-            $genres,
-        );
-
-        return view('movies.index', $viewModel);
+        return view('actors.index', $viewModel);
     }
 
     /**
@@ -81,15 +57,21 @@ class MoviesController extends Controller
      */
     public function show($id)
     {
-        $movie = HTTP::withToken(config('services.tmdb.token'))
-            ->get('https://api.themoviedb.org/3/movie/'.$id.'?append_to_response=credits,videos,images')
+        $actor = Http::withToken(config('services.tmdb.token'))
+            ->get('https://api.themoviedb.org/3/person/'.$id)
             ->json();
 
-        //dump($movie);
+        $social = Http::withToken(config('services.tmdb.token'))
+            ->get('https://api.themoviedb.org/3/person/'.$id.'/external_ids')
+            ->json();
 
-        $viewModel = new MovieViewModel($movie);
+        $credits = Http::withToken(config('services.tmdb.token'))
+            ->get('https://api.themoviedb.org/3/person/'.$id.'/combined_credits')
+            ->json();
 
-        return view('movies.show', $viewModel);
+        $viewModel = new ActorViewModel($actor, $social, $credits);
+
+        return view('actors.show', $viewModel);
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MoviesController;
+use App\Http\Controllers\ActorsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,8 @@ use App\Http\Controllers\MoviesController;
 
 Route::get('/', [MoviesController::class, 'index'])->name('movies.index');
 Route::get('/movies/{movie}', [MoviesController::class, 'show'])->name('movies.show');
+
+Route::get('/actors', [ActorsController::class, 'index'])->name('actors.index');
+Route::get('/actors/page/{page?}', [ActorsController::class, 'index']);
+
+Route::get('/actors/{actor}', [ActorsController::class, 'show'])->name('actors.show');
